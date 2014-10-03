c32fe79b24348f4a4a566a77c3392a68670278d3
Eduard <mamaxropela@gmail.com>
2014-10-03 13:04:02 +0300

    create vector type

---
+++ lab4/lab4/Vector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class Vector
+    {
+        private int[] vector;
+
+        public int Size { get { return vector.Length; } }
+
+        public Vector(int size)
+        {
+            vector = new int[size];
+        }
+
+        public void Generate(int fillDigit)
+        {
+            for (int i = 0; i < Size; i++)
+                vector[i] = fillDigit;
+        }
+
+        #region Operators
+
+        #endregion
+
+        #region Vector manipulation fuctions
+
+        #endregion
+    }
+}
