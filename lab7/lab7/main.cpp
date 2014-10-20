a14f443cf38f2750bfd752134b80625901f3e135
Eduard <mamaxropela@gmail.com>
2014-10-20 19:23:20 +0300

    fix bug with incorrect output

--- lab7/lab7/main.cpp
+++ lab7/lab7/main.cpp
@@ -44,11 +44,11 @@ int main(int argc, char** argv)
 		f3->generate(FILL_NUMBER);
 		char buffer[27];
 		/*strcpy(buffer, "asd");
-		MPI_Recv(buffer, 25, MPI_CHAR, MPI_ANY_SOURCE, PrintTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+		MPI_Recv(buffer, 27, MPI_CHAR, MPI_ANY_SOURCE, PrintTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 		printf(buffer);*/
 		for (int i = 0; i < 18; i++)
 		{
-			MPI_Recv(buffer, 25, MPI_CHAR, MPI_ANY_SOURCE, PrintTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+			MPI_Recv(buffer, 27, MPI_CHAR, MPI_ANY_SOURCE, PrintTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 			printf(buffer);
 		}
 		MPI_Recv(&f1, 1, MPI_INT, 1, RezultTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
@@ -78,35 +78,35 @@ int main(int argc, char** argv)
 void task1() {
 	char buffer[27];
 	strcpy(buffer, "Task1 started\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	Vector *A = new Vector(SIZE),
 		*B = new Vector(SIZE),
 		*C = new Vector(SIZE);
 	Matrix *MA = new Matrix(SIZE),
 		*MZ = new Matrix(SIZE);
 	strcpy(buffer, "generating vector A ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	A->generate(FILL_NUMBER);
 	MPI_Send(&(A->vector[0]), SIZE, MPI_INT, 3, ResourcesTag, MPI_COMM_WORLD);
 	strcpy(buffer, "generating vector B ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	B->generate(FILL_NUMBER);
 	strcpy(buffer, "generating vector C ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	C->generate(FILL_NUMBER);
 	strcpy(buffer, "generating matrix MA ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MA->generate(FILL_NUMBER);
 	MPI_Send(&(MA->matrix[0][0]), SIZE*SIZE, MPI_INT, 2, ResourcesTag, MPI_COMM_WORLD);
 	strcpy(buffer, "generating matrix MZ ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MZ->generate(FILL_NUMBER);
 	strcpy(buffer, "Calculating F1 ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	int d = (*B * *C) + (*A * *B) + (*C * *(*B * *(*MA * *MZ)));
 	delete A, B, C, MA, MZ;
 	strcpy(buffer, "Task1 finished\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MPI_Send(&d, 1, MPI_INT, 0, RezultTag, MPI_COMM_WORLD);
 }
 
@@ -114,18 +114,18 @@ void task1() {
 void task2() {
 	char buffer[27];
 	strcpy(buffer, "Task2 started\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	Matrix *MA = new Matrix(SIZE),
 		*MB = new Matrix(SIZE);
 	MPI_Recv(&(MA->matrix[0][0]), SIZE*SIZE, MPI_INT, 1, ResourcesTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 	strcpy(buffer, "generating matrix MB ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MB->generate(FILL_NUMBER);
 	strcpy(buffer, "Calculating F2 ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	Matrix *MC = (*MA->transpose() * *MB)->sort();
 	strcpy(buffer, "Task2 finished\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MPI_Send(&(MC->matrix[0][0]), SIZE*SIZE, MPI_INT, 0, RezultTag, MPI_COMM_WORLD);
 	delete MA, MB, MC;
 }
@@ -134,26 +134,26 @@ void task2() {
 void task3() {
 	char buffer[27];
 	strcpy(buffer, "Task3 started\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	Vector *A = new Vector(SIZE),
 		*M = new Vector(SIZE);
 	Matrix *MC = new Matrix(SIZE),
 		*MM = new Matrix(SIZE);
 	MPI_Recv(&(A->vector[0]), SIZE, MPI_INT, 1, ResourcesTag, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 	strcpy(buffer, "generating vector M ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	M->generate(FILL_NUMBER);
 	strcpy(buffer, "generating matrix MC ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MC->generate(FILL_NUMBER);
 	strcpy(buffer, "generating matrix MM ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MM->generate(FILL_NUMBER);
 	strcpy(buffer, "Calculating F3 ...\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	Vector *D = *((*A + *M)->sort()) * *((*MC * *MM)->transpose());
 	strcpy(buffer, "Task3 finished\n");
-	MPI_Send(buffer, 25, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
+	MPI_Send(buffer, 27, MPI_CHAR, 0, PrintTag, MPI_COMM_WORLD);
 	MPI_Send(&(D->vector[0]), SIZE, MPI_INT, 0, RezultTag, MPI_COMM_WORLD);
 	delete D, A, M, MC, MM;
 }
