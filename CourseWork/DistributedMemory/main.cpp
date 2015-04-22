e874e3ceec883c6d02466b9329bfe35ce8dc0f5f
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-04-21 20:56:36 -0700

    add empty function to tasks

--- CourseWork/DistributedMemory/main.cpp
+++ CourseWork/DistributedMemory/main.cpp
@@ -1,9 +1,49 @@
+/*
+---------------------------------------------------------------------------------
+--              Course Work. Part #3
+--      Name      Pashchuk Eduard Fedorovich
+--      Group     IP-22
+--      Date      21.04.2014
+--      Task	  MA = MB*MC*alpha + (E*T)*MK
+---------------------------------------------------------------------------------
+*/
+
 #include <mpi.h>
 #include<iostream>
 
 #include "data.h"
 
+#define SIZE 10
+#define FILL_NUMBER 1
+
+#define PrintTag 1
+#define ResourcesTag 2
+#define RezultTag 3
+
 int main(int argc, char* argv[])
 {
+	int rank;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	return 0;
+}
+
+void firstTask()
+{
+	
+}
+
+void lastTask()
+{
+	
+}
+
+void oneTask()
+{
+	
+}
+
+void otherTask()
+{
+	
 }
