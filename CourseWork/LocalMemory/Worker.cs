c5286d33a9ffd311d22c4a707096478efdb7fe5b
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-04-28 16:55:59 +0300

    small changes

--- CourseWork/LocalMemory/Worker.cs
+++ CourseWork/LocalMemory/Worker.cs
@@ -18,7 +18,7 @@ namespace LocalMemory
 		private int[] E, T;
 		private int alpha, ET_result;
 
-		private ManualResetEvent _input1, _inputP;
+		private ManualResetEvent _input1, _inputP, _ET_finish;
 		private ManualResetEvent[] _vectorMultiply, _finishCalculations;
 		private static object _lock = new object();
 		public Worker(int procCount)
@@ -35,13 +35,16 @@ namespace LocalMemory
 
 			_input1 = new ManualResetEvent(false);
 			_inputP = new ManualResetEvent(false);
+			_ET_finish = new ManualResetEvent(false);
 			_vectorMultiply = new ManualResetEvent[P];
 			_finishCalculations = new ManualResetEvent[P];
+
 			for (int i = 0; i < _vectorMultiply.Length; i++)
 				_vectorMultiply[i] = new ManualResetEvent(false);
 			for (int i = 0; i < _finishCalculations.Length; i++)
 				_finishCalculations[i] = new ManualResetEvent(false);
-
+			_vectorMultiply[0].Set();
+			_finishCalculations[0].Set();
 		}
 
 		public Task Start()
@@ -76,7 +79,32 @@ namespace LocalMemory
 			fill(T);
 			_input1.Set();
 			_inputP.WaitOne();
-			calculation(0);
+
+			int[,] MC_copy;
+			int alpha_copy;
+			lock (_lock)
+			{
+				MC_copy = copy(MC);
+				alpha_copy = alpha;
+			}
+			int ET_sum = 0, ET_result_copy = 0;
+			for (int i = getStart(1); i < getEnd(1); i++)
+				ET_sum += E[i] * T[i];
+			Interlocked.Add(ref ET_result, ET_sum);
+			WaitHandle.WaitAll(_vectorMultiply);
+			_ET_finish.Set();
+			Interlocked.Exchange(ref ET_result_copy, ET_result);
+			for (int i = getStart(1); i < getEnd(1); i++)
+				for (int j = 0; j < N; j++)
+				{
+					int sum = 0;
+					for (int k = 0; k < N; k++)
+						sum += MB[i, k] * MC_copy[k, j];
+					sum *= alpha_copy;
+
+					MA[i, j] = sum + MK[i, j] * ET_result_copy;
+				}
+
 			WaitHandle.WaitAll(_finishCalculations);
 			Console.WriteLine("Task {0,2:D} finished!!!", 1);
 			for (int i = 0; i < MA.GetLength(0); i++)
@@ -143,7 +171,7 @@ namespace LocalMemory
 				ET_sum += E[i] * T[i];
 			Interlocked.Add(ref ET_result, ET_sum);
 			_vectorMultiply[id].Set();
-			WaitHandle.WaitAll(_vectorMultiply);
+			_ET_finish.WaitOne();
 			Interlocked.Exchange(ref ET_result_copy, ET_result);
 			for (int i = getStart(id + 1); i < getEnd(id + 1); i++)
 				for (int j = 0; j < N; j++)
