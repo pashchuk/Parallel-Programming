248eddb5bc7b9d38f292150769b448cc1473c02f
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-04-28 18:41:57 +0300

    remove needless using directives

--- CourseWork/LocalMemory/Program.cs
+++ CourseWork/LocalMemory/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
 //MA = MB*MC*alpha + (E*T)*MK
